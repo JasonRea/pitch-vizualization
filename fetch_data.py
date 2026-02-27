54b94e16de69fe7afd5c374e52ad5ab3b11f3a12
Jason Rea <jasonrea0921@gmail.com>
2026-02-27 00:12:06 -0500

    feat: fetch pitch data by date

---
+++ fetch_data.py
@@ -0,0 +1,31 @@
+import pybaseball
+import pandas as pd
+
+pybaseball.cache.enable()
+
+
+'''
+start_dt, end_dt -> Date given by YYYY-MM-DD
+pitcher -> First, last of pitcher (eg. Paul Skenes, Tarik Skubal, Huascar Brazoban) really tried to sneak in skubal there
+'''
+def fetch_pitch_data(start_dt: str, pitcher: str, end_dt: str | None = None) -> pd.DataFrame:
+    
+    try:
+        first, last = pitcher.split(" ")
+
+        # Fuzzy search so special chars in names dont give us a hard time
+        player_id = pybaseball.playerid_lookup(last, first, fuzzy=True)['key_mlbam'].head(1).item() 
+        
+        if not end_dt:
+            end_dt = start_dt
+
+        daily_stats = pybaseball.statcast_pitcher(start_dt=start_dt, end_dt=end_dt, player_id=player_id)
+
+        return daily_stats
+    except:
+        raise RuntimeError
+
+if __name__ == "__main__":
+    
+    df = fetch_pitch_data(start_dt="2026-02-24", pitcher="Ranger Suarez")
+    print(df.head())
