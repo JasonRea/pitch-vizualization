9522b20dd919c4e679174f9bf93339ba3d59e034
Jason Rea <jasonrea0921@gmail.com>
2026-03-02 16:30:08 -0500

    feat: update cli to support builder interface

--- run_vizualization.py
+++ run_vizualization.py
@@ -1,11 +1,11 @@
 import sys
-from pitch_vizualization import *
+from vizualization_builder import *
 
 def print_usage():
     print("Usage: python3 run_vizualization.py [option] [argument]\n")
     print("Options:")
-    print("  -d <date> <pitcher> [quality] [filename]        Render all pitches from an outing on the given day")
-    print('                                                  Example: python3 run_vizualization.py -d "2026-02-24" "Ranger Suarez" "high_quality" "Ranger_Suarez_2026024"')
+    print("  -d <date> <pitcher> [quality]                   Render all pitches from an outing on the given day")
+    print('                                                  Example: python3 run_vizualization.py -d "2026-02-24" "Ranger Suarez" "high_quality"')
     sys.exit(1)
 
 if __name__ == '__main__':
@@ -14,13 +14,16 @@ if __name__ == '__main__':
     arg_1 = sys.argv[2] if len(sys.argv) > 2 else None
     arg_2 = sys.argv[3] if len(sys.argv) > 3 else None
     arg_3 = sys.argv[4] if len(sys.argv) > 4 else None
-    arg_4 = sys.argv[5] if len(sys.argv) > 5 else None
 
     try:
         match flag:
             case "-d":
-                Pitches = construct_pitch_vizualization(arg_1, arg_2)
-                render_scene(Pitches, quality=arg_3, filename=arg_4)
+                scene_class = (
+                    VizualizationBuilder()
+                    .load_pitches(date=arg_1, pitcher=arg_2)
+                    .buildm_pitches()
+                )
+                VizualizationBuilder.render(scene_class, quality=arg_3, filename=f"{arg_2} {arg_1}")
 
             case _:
                 print_usage()
